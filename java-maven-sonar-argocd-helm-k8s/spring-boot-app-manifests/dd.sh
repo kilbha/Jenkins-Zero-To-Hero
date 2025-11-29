c1af96036d4c36084e66a2b7263f2952f10bb705
kilbha <bhargav.mech12@gmail.com>
2025-11-29 12:55:21 +0530

    Added jenkins file223

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/dd.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/dd.sh
@@ -1,2 +1,2 @@
 BUILD_NUMBER=2
-sed -i "s|(image: bhars77/spring-boot-app:).*|\\1${BUILD_NUMBER}|g" deployment.yml
+sed -i "s|(image: bhars77/spring-boot-app:).*|\\1${BUILD_NUMBER}|g" deployment.ymll
