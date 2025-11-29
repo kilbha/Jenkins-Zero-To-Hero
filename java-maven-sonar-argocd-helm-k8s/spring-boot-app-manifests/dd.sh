edd50c9b6802705222ee6b50e734d2aaec85e334
kilbha <bhargav.mech12@gmail.com>
2025-11-29 18:29:39 +0530

    UPdated Jenkins File

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/dd.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/dd.sh
@@ -1,2 +1,8 @@
-BUILD_NUMBER=2
-sed -i "s|(image: bhars77/spring-boot-app:).*|\\1${BUILD_NUMBER}|g" deployment.ymll
+BUILD_NUMBER=8
+# sed -i "s|(image: bhars77/spring-boot-app:).*|\1${BUILD_NUMBER}|g" deployment.yml
+
+# sh '''
+#     sed -i "s/image: bhars77\/spring-boot-app:.*/image: bhars77\/spring-boot-app:${BUILD_NUMBER}/g" deployment.yml
+# '''
+
+sed -i "s|image: bhars77/spring-boot-app:.*|image: bhars77/spring-boot-app:${BUILD_NUMBER}|g" deployment.yml
